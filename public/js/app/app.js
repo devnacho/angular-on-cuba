97b1c928ca3ff7f2c16f1d6602107d7a3ab313ee
Esteban Pastorino <ejpastorino@gmail.com>
2014-09-08 17:49:27 -0400

    html5 pushstate

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,13 +1,14 @@
 (function(){
   var app = angular.module('myBlog', ['ui.router']);
 
-  app.config(function($stateProvider, $urlRouterProvider) {
+  app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true)
 
-    $urlRouterProvider.otherwise('/admin');
+    $urlRouterProvider.otherwise('');
 
     $stateProvider
         .state('new-article', {
-            url: '/articles/new',
+            url: '/admin/articles/new',
             templateUrl: '/js/app/partials/articles/new.html'
         })
 
