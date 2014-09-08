6bfb91aef3476321a88d48daf54d9bfb1797fb96
Esteban Pastorino <ejpastorino@gmail.com>
2014-09-08 17:17:25 -0400

    remove bundler and extra parenthesis

--- app.rb
+++ app.rb
@@ -1,10 +1,7 @@
-require 'bundler'
 require 'cuba'
 require "cuba/render"
 
-Bundler.require
-
-Cuba.plugin(Cuba::Render)
+Cuba.plugin Cuba::Render
 Cuba.settings[:render][:template_engine] = "haml"
 
 Dir["./lib/**/*.rb"].each     { |rb| require rb }
