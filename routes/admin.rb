bab9bc7468c63f043c677608b5b7b8ce4a6412b5
Esteban Pastorino <ejpastorino@gmail.com>
2014-09-10 17:16:32 -0400

    move admin views around, to keep it modular

--- routes/admin.rb
+++ routes/admin.rb
@@ -1,9 +1,11 @@
 class Admin < Cuba
 
+  settings[:render][:views] = File.expand_path("../views/admin", __dir__)
+
   define do
 
     on get do
-      res.write view("/admin")
+      res.write view("app")
     end
 
   end
