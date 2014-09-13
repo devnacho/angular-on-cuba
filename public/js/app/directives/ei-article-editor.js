0d794cd1cb82f546795f62bcf4c2f0e5a9db7046
jgnatch <nachojgutierrez@gmail.com>
2014-09-13 17:48:04 -0300

    idea of markdown help

--- public/js/app/directives/ei-article-editor.js
+++ public/js/app/directives/ei-article-editor.js
@@ -5,6 +5,27 @@ var directive = function() {
         templateUrl: '/partials/directives/ei_article_editor.html',
         scope: {
           content: '='
+        },
+        link: function(scope, element, attrs) {
+          //scope.mdHeaders = ["# H1","## H2","### H3","#### H4","##### H5", "###### H6"];
+          scope.mdHeaders = [{syntax: "# H1", insertText: "# Insert your H1 here"},
+                            {syntax: "## H2", insertText: "## Insert your H2 here"},
+                            {syntax: "### H3", insertText: "### Insert your H3 here"},
+                            {syntax: "#### H4", insertText: "#### Insert your H4 here"},
+                            {syntax: "##### H5", insertText: "##### Insert your H5 here"},
+                            {syntax: "###### H6", insertText: "###### Insert your H6 here"}];
+
+          scope.insert = function(syntax) {
+            if(scope.content == undefined){
+              scope.content = syntax.insertText;
+            } else if(scope.content=== ""){
+              scope.content += syntax.insertText;
+            } else if(scope.content.slice(-1) === "\n"){
+              scope.content += syntax.insertText;
+            } else {
+              scope.content += "\n"+ syntax.insertText;
+            }
+          };
         }
     };
 };
