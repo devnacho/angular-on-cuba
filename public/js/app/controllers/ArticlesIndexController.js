ef552ae44503ef792659b40b9fa634d2d101a01e
jgnatch <nachojgutierrez@gmail.com>
2014-09-22 20:32:13 -0300

    WIP remove articles

--- public/js/app/controllers/ArticlesIndexController.js
+++ public/js/app/controllers/ArticlesIndexController.js
@@ -1,5 +1,11 @@
 function ArticlesIndexController($scope, Article){
     $scope.articles = Article.query();
+
+    $scope.remove = function (index) {
+      console.log($scope.articles[index]);
+      $scope.articles[index].$remove();
+    }
+
 }
 
 angular
