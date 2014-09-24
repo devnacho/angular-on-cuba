8c78c6d69ea19ceeb727fefb392d94beb9bf77dd
Esteban Pastorino <ejpastorino@gmail.com>
2014-09-24 11:41:48 -0400

    update article to use array for tags

--- filters/create_article.rb
+++ filters/create_article.rb
@@ -4,5 +4,16 @@ class CreateArticle < Scrivener
   def validate
     assert_present :title
     assert_present :body
+
+    assert_tags
+  end
+
+  private
+  def assert_tags
+    error = [:tags, :invalid_format]
+    assert tags.is_a?(Array), error
+    tags.each do |tag|
+      assert !tag.to_s.empty?, error
+    end
   end
 end
--- models/article.rb
+++ models/article.rb
@@ -1,20 +1,17 @@
 class Article < Ohm::Model
   include Ohm::Timestamps
+  include Ohm::DataTypes
   attribute :title
   attribute :body
-  attribute :tags
-  index :tagged
+  attribute :tags, Type::Array
+  index :tags
 
   def to_hash
     super.merge title: title,
                 body: body,
                 tags: tags,
-                tagged: tagged,
                 created_at: created_at.iso8601,
                 updated_at: updated_at.iso8601
   end
 
-  def tagged
-    tags.to_s.split(/\s*,\s*/).uniq
-  end
 end
