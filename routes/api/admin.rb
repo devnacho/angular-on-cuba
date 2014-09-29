d9704a62c73431ad71e3f90e7ddf4ab09fcbe3f9
Esteban Pastorino <ejpastorino@gmail.com>
2014-09-29 12:47:27 -0400

    also protect api

--- routes/api/admin.rb
+++ routes/api/admin.rb
@@ -1,57 +1,59 @@
 require_relative '../api'
 class Api::Admin < Cuba
   define do
-    on 'articles' do
+    on authenticated(User) do
+      on 'articles' do
 
-      on root do
-        on get do
-          json Article.all.sort_by(:created_at, order: "DESC")
+        on root do
+          on get do
+            json Article.all.sort_by(:created_at, order: "DESC")
+          end
+
+          on post do
+            publisher = Publisher.new req.params
+
+            on publisher.valid? do
+              article = Article.create publisher.attributes
+              res.status = 201
+              json article
+            end
+
+            on default do
+              res.status = 422
+              json publisher.errors
+            end
+          end
         end
 
-        on post do
-          publisher = Publisher.new req.params
+        on ':id' do |id|
+          article = Article[id]
 
-          on publisher.valid? do
-            article = Article.create publisher.attributes
-            res.status = 201
+          on get do
             json article
           end
 
-          on default do
-            res.status = 422
-            json publisher.errors
+          on put do
+            publisher = Publisher.new req.params
+
+            on publisher.valid? do
+              article.update publisher.attributes
+              json article
+            end
+
+            on default do
+              res.status = 422
+              json publisher.errors
+            end
+          end
+
+          on delete do
+            article.delete
+
+            res.status = 204
           end
         end
+
       end
-
-      on ':id' do |id|
-        article = Article[id]
-
-        on get do
-          json article
-        end
-
-        on put do
-          publisher = Publisher.new req.params
-
-          on publisher.valid? do
-            article.update publisher.attributes
-            json article
-          end
-
-          on default do
-            res.status = 422
-            json publisher.errors
-          end
-        end
-
-        on delete do
-          article.delete
-
-          res.status = 204
-        end
-      end
-
     end
   end
 end
