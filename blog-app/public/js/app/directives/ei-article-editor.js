b13f6a6399181c398349c9c57389e3a1fd62c55c
Esteban Pastorino <ejpastorino@gmail.com>
2014-10-19 15:01:20 -0300

    more extensible markdown help

--- blog-app/public/js/app/directives/ei-article-editor.js
+++ blog-app/public/js/app/directives/ei-article-editor.js
@@ -9,12 +9,13 @@ function eiArticleEditor() {
         link: function(scope, element, attrs) {
           scope.showHelp = false;
           scope.preview = false;
-          scope.mdHeaders = [{syntax: "# H1", insertText: "# Insert your H1 here"},
-                            {syntax: "## H2", insertText: "## Insert your H2 here"},
-                            {syntax: "### H3", insertText: "### Insert your H3 here"},
-                            {syntax: "#### H4", insertText: "#### Insert your H4 here"},
-                            {syntax: "##### H5", insertText: "##### Insert your H5 here"},
-                            {syntax: "###### H6", insertText: "###### Insert your H6 here"}];
+          scope.mdHints= { "Headers": [{syntax: "# H1", insertText: "# Insert your H1 here"},
+                                        {syntax: "## H2", insertText: "## Insert your H2 here"},
+                                        {syntax: "### H3", insertText: "### Insert your H3 here"},
+                                        {syntax: "#### H4", insertText: "#### Insert your H4 here"},
+                                        {syntax: "##### H5", insertText: "##### Insert your H5 here"},
+                                        {syntax: "###### H6", insertText: "###### Insert your H6 here"}]
+                          }
 
           scope.togglePreview = function(){
             scope.preview = !scope.preview;
