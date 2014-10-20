f25794a0fcafbf772d47ac81849dc7fc7f9b08ff
jgnatch <nachojgutierrez@gmail.com>
2014-10-20 20:33:29 -0300

    added remove article exercise

--- blog-app/public/js/app/controllers/admin/AdminArticlesIndexController.js
+++ blog-app/public/js/app/controllers/admin/AdminArticlesIndexController.js
@@ -2,6 +2,12 @@ function AdminArticlesIndexController(Article){
 
     this.articles = Article.query();
 
+    var that = this;
+
+    this.remove = function (index) {
+      that.articles[index].$remove();
+      that.articles.splice(index, 1);
+    }
 }
 
 angular
