7c33c353c2ff5f383c7e313f07c27f76061d1dd3
jgnatch <nachojgutierrez@gmail.com>
2014-10-20 21:00:56 -0300

    agrego ejemplo de ng show

---
+++ ejemplos/angular/3-directives/ng-show/app.js
@@ -0,0 +1,29 @@
+angular.module('app', []);
+
+function IndexCtrl(){
+  this.articles = [
+    {
+      title: "Nepal's worst snowstorm disaster",
+      published: true
+    },
+    {
+      title: "Comet Siding Spring",
+      published: true
+    },
+    {
+      title: "Nikola Tesla",
+      published: false
+    }
+  ]
+
+  var that = this;
+
+  this.removeName = function (index) {
+    that.names.splice(index, 1)
+  }
+}
+
+
+angular
+  .module('app')
+  .controller('IndexCtrl', IndexCtrl);
